724928be007f259dbcd02f268c7f9e51cf2198a9
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2023-05-18 14:08:08 -0300

    create app layout mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,74 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  // eslint-disable-next-line no-lone-blocks
+  {
+    /* foi colocado px-9 (padding 32px) para dar um respiro da frase nas laterais da tela, em dispositivos menores (2400X1080) vai ter que utilizar padding px-20 galaxy s20 fe */
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-gray-50">Rocketseat</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-9 py-9"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      {/* como não foi criado com css e sim por uma importação SVG tive que colocar um bottom para posicionar a Stripes (meu dispositivo galaxy s20 fe ficou bom, mas para dispositivos maiores (1080x2400)vai ter que utilizar bottom-2) */}
+      <StyledStripes className="absolute bottom-5 left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200 ">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="light" />
-    </View>
+    </ImageBackground>
   )
 }
